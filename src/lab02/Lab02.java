ddc04a964d688fa9a635eaacfeed0057cfea71a7
Ronald Tran <Ronald Tran@LAPTOP-I2R04I57>
2022-01-20 16:14:05 -0700

    For LAB02

---
+++ src/lab02/Lab02.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab02;
+
+/**
+ *
+ * @author Ronald Tran
+ */
+public class Lab02 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // This command will print out "Hello world!"
+        System.out.println("Hello world!");
+    }
+    
+}
